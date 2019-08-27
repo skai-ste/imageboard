ddaaf94d0dd45edbb39bccae9eec4637735bc4f9
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-08-27 11:19:23 +0200

    week8. IB: getting data for the images.

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
 const express = require("express");
 const app = express();
+const { getImages } = require("./utils/db");
+
+///////////// FILE UPLOAD BOILERPLATE ///////////
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function(req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    }
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
+///////////// FILE UPLOAD BOILERPLATE ///////////
 
 app.use(express.static("public"));
 
-app.get("/planets", (req, res) => {
-    let planets = [
-        {
-            name: "Pluto",
-            color: "grey/yellow"
-        },
-        {
-            name: "Venus",
-            color: "orange"
-        },
-        {
-            name: "Mars",
-            color: "red"
-        }
-    ];
-    console.log("i hit the planets route");
-    res.json(planets);
+// app.get("/planets", (req, res) => {
+//     let planets = [
+//         {
+//             name: "Pluto",
+//             color: "grey/yellow"
+//         },
+//         {
+//             name: "Venus",
+//             color: "orange"
+//         },
+//         {
+//             name: "Mars",
+//             color: "red"
+//         }
+//     ];
+//     console.log("i hit the planets route");
+//     res.json(planets);
+// });
+
+// {
+//     "images" : [
+//         {
+//             "name" : "bla",
+//             "url" : "example.com"
+//         }
+//     ]
+// }
+
+app.get("/images", (req, res) => {
+    getImages()
+        .then(result => {
+            console.log("Result is: ", result);
+            res.json({
+                images: result
+            });
+        })
+        .catch(err => {
+            console.log("ERR", err);
+        });
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    // req.file - the file that was just uploaded
+    // req.body - refers to the values we type in the input fiels
+    if (req.file) {
+        res.json({
+            success: true
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
 });
 
 app.listen(8080, () => console.log("My image board server is UP!"));
--- public/js/script.js
+++ public/js/script.js
@@ -4,29 +4,57 @@ console.log("sanity check!!!");
     new Vue({
         el: "#main",
         data: {
-            name: "Sassafras", // these properties are reactive
+            images: [],
             seen: true,
-            planets: [],
-            myClassName: "highlight"
+            title: "",
+            description: "",
+            username: "",
+            file: null
         },
         mounted: function() {
             console.log("my vue has mounted!");
-            console.log("Planet is: ", this.planets);
             var me = this;
-            axios.get("/planets").then(function(response) {
-                console.log("this.planets in then", this.planets);
-                console.log("me.planets in then", me.planets);
+            axios.get("/images").then(function(response) {
+                console.log("this.images in then", this.images);
+                console.log("me.images in then", me.images);
                 console.log("This is my response!", response.data);
-                me.planets = response.data;
+                me.images = response.images;
             });
         },
         methods: {
-            myFunction: function(planetName) {
-                console.log("myFunction is runing!", planetName);
+            handleClick: function(e) {
+                e.preventDefault();
+                console.log("this: ", this);
+
+                var formData = new FormData();
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+                //you have to use method and loop through it to see what is inside
+
+                axios
+                    .post("/upload", formData)
+                    .then(function(resp) {
+                        console.log("resp from post /upload: ", resp);
+                    })
+                    .catch(function(err) {
+                        console.log("err in post /upload: ", err);
+                    });
             },
-            myNextMethod: function() {
-                console.log("another method");
+            handleChange: function(e) {
+                // console.log("handleChange is running!!!!");
+                // console.log("file: ", e.target.files[0]);
+                this.file = e.target.files[0];
             }
         }
+        // methods: {
+        //     myFunction: function(planetName) {
+        //         console.log("myFunction is runing!", planetName);
+        //     },
+        //     myNextMethod: function() {
+        //         console.log("another method");
+        //     }
+        // }
     });
 })();
