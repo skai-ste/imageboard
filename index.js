1a1bcfc9ae4948b2498eae8ed8872c9d23073953
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-08-27 15:31:24 +0200

    week8. IB: start of part2.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const { getImages } = require("./utils/db");
+const s3 = require("./s3");
+const config = require("./config");
 
 ///////////// FILE UPLOAD BOILERPLATE ///////////
 const multer = require("multer");
@@ -41,9 +43,13 @@ app.get("/images", (req, res) => {
         });
 });
 
-app.post("/upload", uploader.single("file"), (req, res) => {
+app.post("/upload", uploader.single("file"), s3.upload, (req, res) => {
     // req.file - the file that was just uploaded
     // req.body - refers to the values we type in the input fiels
+    const { filename } = req.file;
+    const url = config.s3url + filename; //if you got here you have url of img and all other information
+    const { title, username, description } = req.body; // you gotta put this information in database, you will do data base query INSERT : title, username, description, url. Send that back into response
+
     if (req.file) {
         res.json({
             success: true
@@ -54,5 +60,17 @@ app.post("/upload", uploader.single("file"), (req, res) => {
         });
     }
 });
+// after amazon is complete you should put your images. Because if amazon fales you have no image!
+//you should UNSHIFT image not push it. It's then infront of your images array
+
+// axios.post('./upload', formData).then(funciotn(resp) {
+//     var img = resp.data;
+// });
+//
+// {
+//     image: 'https://whatever.com/fun.jpg',
+//     title: "",
+//     id: 11
+// }
 
 app.listen(8080, () => console.log("My image board server is UP!"));
---
+++ s3.js
@@ -0,0 +1,45 @@
+const aws = require("aws-sdk");
+const fs = require("fs");
+
+let secrets;
+if (process.env.NODE_ENV == "production") {
+    secrets = process.env; // in prod the secrets are environment variables
+} else {
+    secrets = require("./secrets"); // in dev they are in secrets.json which is listed in .gitignore
+}
+
+const s3 = new aws.S3({
+    accessKeyId: secrets.AWS_KEY,
+    secretAccessKey: secrets.AWS_SECRET
+});
+
+exports.upload = function(req, res, next) {
+    if (!req.file) {
+        return res.send(500);
+    }
+    const { filename, mimetype, size, path } = req.file;
+
+    const promise = s3
+        .putObject({
+            Bucket: "spicedling",
+            ACL: "public-read", //everyone who has url of the img can read it
+            Key: filename,
+            Body: fs.createReadStream(path),
+            ContentType: mimetype,
+            ContentLength: size
+        })
+        .promise();
+
+    promise
+        .then(() => {
+            // it worked!!!
+            next();
+            //that means the file is at amazon, and if it there the url is s2/wwwmaazon.com/name of your bucket/maybeimgname
+            fs.unlink(path, () => {}); //when it get too many files
+        })
+        .catch(err => {
+            // uh oh
+            console.log(err);
+            res.sendStatus(500);
+        });
+};
